8ff4c9c1ba5c1c0852b82141b9be5dd39de0602d
Nicholas Mawulorm <nmawulorm@gmail.com>
2025-09-18 09:31:09 +0000

    cxkfkfd

--- City Cruz Rental/DialogBoxes/AddCustomerDialog.vb
+++ City Cruz Rental/DialogBoxes/AddCustomerDialog.vb
@@ -23,7 +23,7 @@ Public Class AddCustomerDialog
         validator.ClearEntries()
         validator.AddEntry(txtFirstName, Label1, FieldValidator.FieldType.NAME)
         validator.AddEntry(txtLastName, Label7, FieldValidator.FieldType.NAME)
-        validator.AddEntry(txtLicenseNumber, Label1, FieldValidator.FieldType.NAME)
+        validator.AddEntry(txtLicenseNumber, Label9, FieldValidator.FieldType.NUMBER_PLATE)
         validator.AddEntry(txtEmail, Label10, FieldValidator.FieldType.NUMBER_PLATE)
         validator.AddEntry(txtPostalCode, Label11, FieldValidator.FieldType.NAME)
         validator.AddEntry(txtPhone, Label11, FieldValidator.FieldType.PHONE)
